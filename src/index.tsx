73b8230c1332dba9005f7bd8d8bfa5685f2fde21
bang9 <gusrn1423@naver.com>
2024-02-29 13:47:36 +0900

    chore: remove unused files

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,3 @@
-import { NativeModules, Platform } from 'react-native';
-
-const LINKING_ERROR =
-  `The package '@sendbird/notifications-extension' doesn't seem to be linked. Make sure: \n\n` +
-  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-  '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
-
-const NotificationsExtension = NativeModules.NotificationsExtension
-  ? NativeModules.NotificationsExtension
-  : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-
-export function multiply(a: number, b: number): Promise<number> {
-  return NotificationsExtension.multiply(a, b);
+export function _() {
+  // noop
 }
