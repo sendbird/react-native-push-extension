be9cf9bfd45915dafc431e5090095056a83e5ddc
bang9 <gusrn1423@naver.com>
2024-02-29 14:33:29 +0900

    chore: apply to example project

--- example/ios/NotificationsExtensionExample/AppDelegate.mm
+++ example/ios/NotificationsExtensionExample/AppDelegate.mm
@@ -1,16 +1,18 @@
 #import "AppDelegate.h"
+#import "SendbirdNotificationHelper.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <UserNotifications/UserNotifications.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"NotificationsExtensionExample";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+  // NOTE: set app group to share device token
+  [SendbirdNotificationHelper setAppGroup:@"group.sample.chat.react-native"];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
@@ -26,6 +28,12 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+  // NOTE: pass device token
+  [SendbirdNotificationHelper didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
 }
 
 @end
